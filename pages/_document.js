b5e84223e84441fed9efe23f032975d17b1b4c54
Tasnim Zotder <tasnimzotder@gmail.com>
2021-09-27 08:10:16 -0700

    Initial commit

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
